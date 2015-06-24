c5eb23f9c9fd0e985f3ef92e008a0abfb4138748
Talles L <talleslasmar@gmail.com>
2015-06-24 19:36:53 -0300

    existing code

---
+++ MarkdownHtmlHelper/MarkdownHtmlHelper.cs
@@ -0,0 +1,42 @@
+﻿namespace MarkdownHtmlHelper
+{
+    using System.IO;
+    using System.Web;
+    using System.Web.Mvc;
+    using MarkdownSharp;
+
+    /// <summary>
+    /// An HTML Helper that renders markdown.
+    /// </summary>
+    public static class MarkdownHelper
+    {
+        /// <summary>
+        /// SO's markdown parser.
+        /// </summary>
+        private static readonly Markdown _parser = new Markdown();
+
+        /// <summary>
+        /// Reads the content of the given file path, interprets as markdown and renders the equivalent HTML.
+        /// </summary>
+        /// <param name="helper">Html helper</param>
+        /// <param name="path">The file with the markdown content to be rendered</param>
+        /// <returns>The rendered HTML</returns>
+        public static IHtmlString Markdown(this HtmlHelper helper, string path)
+        {
+            var view = helper.ViewContext.View as RazorView;
+            if (view == null)
+            {
+                throw new NotARazorViewException();
+            }
+            else
+            {
+                var viewPath = helper.ViewContext.HttpContext.Server.MapPath(view.ViewPath);
+                var dir = Path.GetDirectoryName(viewPath);
+                var fullpath = Path.Combine(dir, path);
+                var unparsed = File.ReadAllText(fullpath);
+                var parsed = _parser.Transform(unparsed);
+                return new MvcHtmlString(parsed);
+            }
+        }
+    }
+}
---
+++ MarkdownHtmlHelper/NotARazorViewException.cs
@@ -0,0 +1,15 @@
+﻿namespace MarkdownHtmlHelper
+{
+    using System;
+
+    /// <summary>
+    /// Exception thrown when a view which isn't a Razor view is used with the helper.
+    /// </summary>
+    public class NotARazorViewException : Exception
+    {
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        internal NotARazorViewException() : base("MarkdownHtmlHelper helper only works with Razor views!") { }
+    }
+}
